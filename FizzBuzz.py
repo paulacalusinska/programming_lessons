dc382be55e80f90a64b8c02af833ec1a33a83787
Paula <paula.calusinska@gmail.com>
2022-02-18 20:29:57 +0100

    files from previous lessons

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 def fizzBuzz(n):
     # Write your code here
     for i in range(1, n+1):
-        string = "asdasd"
+    
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         if i % 3 == 0 and i % 5 != 0:

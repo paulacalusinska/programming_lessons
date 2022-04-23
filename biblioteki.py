0bc66b68435c8f01189c14b2890aafa10c3747d5
Paula <paula.calusinska@gmail.com>
2022-04-23 21:03:26 +0200

    dodałam zadanka domowe

--- biblioteki.py
+++ biblioteki.py
@@ -10,5 +10,23 @@ def listdir_if_dir(path):
 listdir_if_dir(path)
 
 # wypisać wszystkie pliki z katalogu w kolejnosci alfabetycznej
+def listdir_in_order(path):
+    if os.path.isdir(path):
+        print(sorted(os.listdir(path)))
+    else:
+        print(f'Path {path} is not directory')
+
 # czy to co jest wewnatrz katalogu to plik czy katalog
+
+dirs = os.listdir(path)
+for file in dirs:
+    print(os.path.isdir(file))
+
 # wypisujemy tez wszystko co jest wewnatrz katalogow
+
+dirs2 = os.listdir(path)
+for file in dirs2:
+    if os.path.isdir(file):
+        print(os.listdir(file))
+    else:
+        print(file)
